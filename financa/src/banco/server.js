6712203a3bcce83f6e5c903afacb292e914503c5
DevGamorim <78799375+DevGamorim@users.noreply.github.com>
2022-06-11 14:46:38 -0300

    adding connect mongodb

---
+++ financa/src/banco/server.js
@@ -0,0 +1,35 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const MONGO_CNSTRING = process.env.MONGO_CNSTRING;
+
+const { MongoClient } = require('mongodb');
+// or as an es module:
+// import { MongoClient } from 'mongodb'
+
+// Connection URL
+const client = new MongoClient(MONGO_CNSTRING);
+
+// Database Name
+const dbName = 'Projeto';
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+  const collection = db.collection('user');
+
+  await collection.insertOne({
+    cpf:10737538910,
+    nome:'Fernando Amorim'
+  });
+  // the following code examples can be pasted here...
+
+  return 'done.';
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
