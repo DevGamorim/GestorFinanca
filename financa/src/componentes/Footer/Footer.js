b7564a41e096b4a658fb61e09e1cdf9388e49671
DevGamorim <78799375+DevGamorim@users.noreply.github.com>
2022-06-13 20:19:21 -0300

    add footer and select_group

---
+++ financa/src/componentes/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+  
+const Footer = () => (
+  <footer className="footer">
+    <center>
+    <p>Contato: @Senai</p>
+    </center>
+    
+  
+  </footer>
+);
+  
+export default Footer;
---
+++ financa/src/componentes/Select_grup/Select_grup.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const select_grup = () =>(
+  <div>
+    aaaaaaccc
+  </div>
+);
+  
+export default select_grup;
